afc87cdb92eba138de27c37eb108a14871d41223
coderonfleek <fikfikky@gmail.com>
2021-10-18 16:33:57 +0100

    Webhook Trigger

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Successfully Deployed <code>A React App</code> to Netlify with CircleCI
+          Successfully Deployed <code> A React App </code> to Netlify with CircleCI
         </p>
         <a
           className="App-link"
